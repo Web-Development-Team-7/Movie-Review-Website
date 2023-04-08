931cdf43a7871055e1da42436284f4f3a18fd6a2
Bebo561 <soto.carlodavid@gmail.com>
2023-04-07 20:04:30 -0400

    Working on login page, learning tailwind

--- Frontend/src/Components/login.tsx
+++ Frontend/src/Components/login.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './login.css'
+import {useState} from 'react';
 
 export default function Login(){
+    var [user, setUser] = useState('');
+    var [password, setPass] = useState('');
+    
     return(
         <React.Fragment>
-           <h1>Hello World</h1>
+           <div className='flex justify-center w-full' id = "LogFlex">
+            <form className=''>
+                <h1>Welcome</h1>
+                <p className="lg">Log In</p>
+                <input id="Loginput" type = "text"  className= 'bg-slate-200' name="Username"  onChange={(e) => setUser(e.target.value)} placeholder = "Enter Username" />
+                <input id="Loginput" type = "password" name ="Password" className='bg-slate-200'  onChange = {(e) => setPass(e.target.value)} placeholder = "Enter Password"/>
+                <button> Sign In With Google</button>
+                <button >Log in</button>
+            </form>
+        </div>
         </React.Fragment>
     )
 }

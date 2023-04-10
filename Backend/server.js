10d7a515184d2152bd7e99cea50f463125683ba4
vladiazouga <vzouga2021@fau.edu>
2023-04-09 22:08:45 -0400

    made API routes

--- Backend/server.js
+++ Backend/server.js
@@ -7,28 +7,23 @@ const cors = require('cors');
 const bcryptjs = require('bcryptjs');
 const axios= require('axios');
 const { get } = require('http');
+const request = require('request');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static('./public'));
 
 //Have option to Sign in Without Google, used to encrypt Passwords
 const saltRounds = 10;
 
-// const options2 = {
-//   method: 'GET',
-//   url: 'https://online-movie-database.p.rapidapi.com/title/get-most-popular-movies',
-//   params: {currentCountry: 'US', purchaseCountry: 'US', homeCountry: 'US'},
-//   headers: {
-//     'X-RapidAPI-Key': '0a51dbb737msh24f7f6ca1389daep1efa5bjsndcf7ac74473d',
-//     'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
-//   }
-// };
+app.use(cors({
+    origin: '*', // allow to server to accept request from different origin
+  }));
 
-// axios.request(options2).then(function (response) {
-//     console.log(response.data);
-// }).catch(function (error) {
-//     console.error(error);
-// });
-
-let getTop50=async ()=>{
-const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+app.get('/getTop/', async function (req, res) {
+    // View all students if no query parameters are provided
+    //let students = await Model.find({last_name:req.params.lastname});
+    const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
   params: {
     api_key: '5e072d084652ab8ef66bf80de30d4235',
     language: 'en-US',
@@ -36,38 +31,39 @@ const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
   },
 });
 const movies = response.data.results.slice(0, 50);
-//console.log(movies)
-}
-//getTop50()
+    res.status(200).send(movies);
+  
+    });
 
 
 
-const request = require('request');
 
-const MOVIEDB_API_KEY = '5e072d084652ab8ef66bf80de30d4235';
-const MOVIE_ID = 505642; // Replace with the ID of the movie you want to get the actors for
 
-// Make a GET request to the moviedb API to get the cast details for the movie
-request.get(`https://api.themoviedb.org/3/movie/${MOVIE_ID}/credits?api_key=${MOVIEDB_API_KEY}`, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
+    
+    
+    app.get('/actors/:movie_id', (req, res) => {
+        const MOVIEDB_API_KEY = '5e072d084652ab8ef66bf80de30d4235';
+    const MOVIE_ID = req.params.movie_id; // Replace with the ID of the movie you want to get the actors for
+      // Make a GET request to the moviedb API to get the cast details for the movie
+      request.get(`https://api.themoviedb.org/3/movie/${MOVIE_ID}/credits?api_key=${MOVIEDB_API_KEY}`, (error, response, body) => {
+        if (error) {
+          console.error(error);
+          res.status(500).send('Internal Server Error');
+          return;
+        }
+    
+        const data = JSON.parse(body);
+    
+        // Extract the list of actors from the API response
+        const actors = data.cast.map(actor => actor.name);
+    
+        // Send the list of actors in the response
+        res.send(`Actors in the movie: ${actors.join(', ')}`);
+      });
+    });
 
-  const data = JSON.parse(body);
 
-  // Extract the list of actors from the API response
-  const actors = data.cast.map(actor => actor.name);
 
-  // Log the list of actors to the console
-  console.log(`Actors in the movie: ${actors.join(', ')}`);
-});
 
-// axios.get(`http://www.omdbapi.com/?i=tt0031885&apikey=a502ab9b`)
-//   .then(response => {
-//     const movie = response.data;
-//     console.log(movie);
-//   })
-//   .catch(error => {
-//     console.error(error);
-//   });
+app.listen(5678); //start the server
+console.log('Server is running...');

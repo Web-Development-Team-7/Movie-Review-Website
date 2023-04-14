a31c1f7aaf7540c0b955ba63d4503ffff4df6226
Bebo561 <soto.carlodavid@gmail.com>
2023-04-14 16:12:36 -0400

    Fixed button

--- Frontend/src/Components/login.tsx
+++ Frontend/src/Components/login.tsx
@@ -50,14 +50,15 @@ export default function Login(){
     return(
         <React.Fragment>
            <div className='flex justify-center w-full' id = "LogFlex">
-           <button id="GoogleLog" onClick={GoogleSignIn} className = "bg-white rounded-md text-center mt-40 h-10 w-48 font-500 absolute z-10 border-black border border-solid">
-                    <img src = './Assets/google.png' className='h-15 w-8 top-1 absolute' alt='Google Logo'></img>Log In With Google</button>
+
             <form className='bg-white flex relative rounded-md border-solid border-2 w-1/3 border-black h-4/5' id="LogForm">
                 <h1 className="text-3xl font-bold mt-10">Welcome</h1>
                 <p className="text-2xl font-medium mt-10">Log In</p>
                 <input id="Loginput" type = "email"  className= 'bg-slate-200 mt-10 h-10 w-64 rounded-lg text-center' name="Email"  onChange={(e) => setEmail(e.target.value)} placeholder = "Enter Email" />
                 <input id="Loginput" type = "password" name ="Password" className='bg-slate-200 mt-5 h-10 w-64 rounded-lg text-center'  onChange = {(e) => setPass(e.target.value)} placeholder = "Enter Password"/>
                 <button id="LogButton" onClick={UserSignIn} className='mt-10 bg-blue-500 h-10 w-64 text-center rounded-lg border-solid border-black border text-black text-lg font-500 ' >Log In</button>
+                <button id="GoogleLog" onClick={GoogleSignIn} className = "bg-white rounded-md text-center mt-96 h-10 w-48 font-500 absolute z-10 border-black border border-solid">
+                    <img src = './Assets/google.png' className='h-15 w-8 top-1 absolute' alt='Google Logo'></img>Log In With Google</button>
                 <h3 className='mt-20'>Don't Have An Account?
                 <Link to='/Signup' id="LinkSign" className='text-blue-600'>Sign Up!</Link></h3>
             </form>

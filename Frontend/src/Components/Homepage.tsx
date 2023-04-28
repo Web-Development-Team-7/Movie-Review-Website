7e25807c7b4c167d61cd6fbf1ade09496346a8f2
vladiazouga <vzouga2021@fau.edu>
2023-04-28 19:09:34 -0400

    homepage css

--- Frontend/src/Components/Homepage.tsx
+++ Frontend/src/Components/Homepage.tsx
@@ -4,6 +4,9 @@ import axios from 'axios'
 import { Link } from 'react-router-dom';
 import Navbar from './navbar';
 
+
+
+
 interface Movie {
   adult: boolean;
   backdrop_path: string;
@@ -26,7 +29,7 @@ let HomePage = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [topMovie, setTopMovie]=useState<Movie[]>([]);
   //const[genre, setGenre] = useState([]);
-  const itemsPerPage = 4;
+  const itemsPerPage = 10;
   
   const totalPages = Math.ceil(topMovie.length / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage;
@@ -64,19 +67,20 @@ let HomePage = () => {
   return (
 <React.Fragment>
   <Navbar/>
-
- 
+  
+  <div className="bg-gray-950 h-screen">
+     <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Top 20 Trending Movies</h1>
   <div id='moviecontainer'>
-  <ul className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+  <ul className="grid grid-cols-1 sm:grid-cols-5 lg:grid-cols-5 gap-5">
     {topMovie
       .filter(movie => movie.backdrop_path)
       .slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage)
       .map((item, index) => (
         <div key={index} className="relative">
           <Link to= {`/details?id=${item.id}`}>
-            <img src={`https://image.tmdb.org/t/p/w400/${item.backdrop_path}`} alt={item.title} className="photo"/>
+            <img src={`https://image.tmdb.org/t/p/w300/${item.backdrop_path}`} alt={item.title} className="photo"/>
 
-          <div className="absolute bottom-3 left-3 right-3 bg-gray-900 bg-opacity-50 py-20 px-6 text-white transition-opacity duration-300 opacity-0 hover:opacity-100">
+          <div className="absolute bottom-3 left-3 right-3 bg-gray-900 bg-opacity-50 py-6 px-10 text-white transition-opacity duration-300 opacity-0 hover:opacity-100">
             <p className="text-xl font-bold">{item.title}</p>
             <p className="text-sm">{item.release_date}</p>
           </div>
@@ -85,22 +89,24 @@ let HomePage = () => {
       ))
     }
   </ul>
-</div>
-
-
-  <div className='pagination'>
+  <div className='pagination bg-gray-950 mb-24'>
     {Array.from({ length: totalPages }, (_, index) => (
       <button
         key={index}
         className={`${
     currentPage === index + 1 ? 'active bg-gray-600' : 'bg-gray-500 hover:bg-gray-600'
-  } text-white px-4 py-2 rounded mx-2`}
+  } text-white px-6 py-3.5 rounded mx-2`}
         onClick={() => handlePageChange(index + 1)}
       >
         {index + 1}
       </button>
     ))}
   </div>
+
+  </div>
+</div>
+
+
 </React.Fragment>
 
   );
